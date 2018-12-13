e21e84492a20709ca3372a6c5e24ae621d26b44c
sosout <sosout@139.com>
2018-12-13 16:19:26 +0800

    fix git status

--- git/status.js
+++ git/status.js
@@ -1,4 +1,5 @@
 const execSync = require('child_process').execSync;
+const chalk = require('chalk');
 
 function getGitStatus() {
   try {
@@ -12,5 +13,21 @@ function getGitStatus() {
 }
 
 const gitStatus = getGitStatus();
-
-console.log(gitStatus);
+if (gitStatus) {
+  // 此git存储库具有未跟踪的文件或未提交的更改
+  console.error(
+    chalk.red(
+      'This git repository has untracked files or uncommitted changes:'
+    ) +
+      '\n\n' +
+      gitStatus
+        .split('\n')
+        .map(line => line.match(/ .*/g)[0].trim())
+        .join('\n') +
+      '\n\n' +
+      chalk.red(
+        'Remove untracked files, stash or commit any changes, and try again.'
+      )
+  );
+  process.exit(1);
+}
